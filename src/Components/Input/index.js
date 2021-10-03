c0ba2ee36846d36c84a34abd54f5838e49ebfbab
MiguelAngelMG42 <MikeRen@outlook.com>
2021-10-03 00:05:26 -0500

    se corrigen los inputs y se agrega la funcionalidad login usando axios y jquery

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -3,12 +3,12 @@ import "./style.scss";
 
 
 const InputGeneric = (props) => {
-    const { textLabel, textInput } = props
+    const { textLabel, placeHolderText, inputType, handler } = props
     return (
         <>
             <div class="form-group">
                 <label for="InputGeneric"><b>{textLabel}</b></label>
-                <input type="input" class="form-control" id="InputGeneric" aria-describedby="Help" placeholder={textInput} />
+                <input type={inputType} class="form-control" id="InputGeneric" aria-describedby="Help" placeholder={placeHolderText} onChange={handler} />
                 <small id="Help" class="form-text text-muted">*Texto valido/Texto invalido</small>
             </div>
         </>
