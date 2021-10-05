180ae5542603e95098a231b2407d6fff54f7cead
Misa-El <mmancillaglz07@gmail.com>
2021-10-05 18:05:21 -0500

    se agregan colores en assets

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import BulletinTable from "../../Components/BulletinTable";
 //import './App.scss';
 
 function Search() {
   return (
     <>
-    <h1>Search</h1>
+    <BulletinTable/>
     </>
   );
 }
