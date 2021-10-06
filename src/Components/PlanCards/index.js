72e41e38f6afdb054fc29cd02b36b965622f5356
Misa-El <mmancillaglz07@gmail.com>
2021-10-05 19:46:34 -0500

    ajuste de iconos planes y tabla

--- src/Components/PlanCards/index.js
+++ src/Components/PlanCards/index.js
@@ -14,9 +14,9 @@ function PlanCards () {
           <CardTitle tag="h1" className="plan text-center mb-3">Básico</CardTitle>
           <CardSubtitle tag="h5" className="price text-center mb-4 ">USD 0.00 / mes*</CardSubtitle>
           <CardText>
-            <p><span class="material-icons md-light">folder</span>20 expedientes personalizados</p>
-            <p><span class="material-icons md-light">timelapse</span>Sin tiempo de expiración</p>
-            <p><span class="material-icons md-light">timer</span>Actualización diaria en la plataforma</p>
+            <p><span class="material-icons">folder_open</span>20 expedientes personalizados</p>
+            <p><span class="material-icons">timelapse</span>Sin tiempo de expiración</p>
+            <p><span class="material-icons">timer</span>Actualización diaria en la plataforma</p>
           </CardText>
           <Button id="elegir" className="btn-elegir">Elegir</Button>
         </CardBody>
@@ -26,10 +26,10 @@ function PlanCards () {
           <CardTitle tag="h1" className="plan text-center mb-3">Profesional</CardTitle>
           <CardSubtitle tag="h5" className="price text-center mb-3 ">USD 40.00  / mes*</CardSubtitle>
           <CardText>
-            <p className="icon "><span class="material-icons md-light">folder</span>20 expedientes personalizados</p>
-            <p><span class="material-icons md-light">timelapse</span>Sin tiempo de expiración</p>
-            <p><span class="material-icons md-light">timer</span>Actualización diaria en la plataforma</p>
-            <p><span class="material-icons md-light">event</span>Tareas personalizadas a los boletines</p>
+            <p><span class="material-icons">folder_open</span>20 expedientes personalizados</p>
+            <p><span class="material-icons">timelapse</span>Sin tiempo de expiración</p>
+            <p><span class="material-icons">timer</span>Actualización diaria en la plataforma</p>
+            <p><span class="material-icons">insert_invitation</span>Tareas personalizadas a los boletines</p>
           </CardText>
           <Button id="elegir2" className="btn-elegir">Elegir</Button>
         </CardBody>
@@ -39,11 +39,11 @@ function PlanCards () {
           <CardTitle tag="h1" className="plan text-center mb-3">Profesional+</CardTitle>
           <CardSubtitle tag="h5" className="price text-center mb-3 ">USD 90.00 / mes*</CardSubtitle>
           <CardText>
-            <p><span class="material-icons md-light">folder</span>20 expedientes personalizados</p>
-            <p><span class="material-icons md-light">timelapse</span>Sin tiempo de expiración</p>
-            <p><span class="material-icons md-light">timer</span>Actualización diaria en la plataforma</p>
-            <p><span class="material-icons md-light">event</span>Tareas personalizadas a los boletines</p>
-            <p><span class="material-icons md-light">perm_phone_msg</span>Soporte personalizado</p>
+            <p><span class="material-icons">folder_open</span>20 expedientes personalizados</p>
+            <p><span class="material-icons">timelapse</span>Sin tiempo de expiración</p>
+            <p><span class="material-icons">timer</span>Actualización diaria en la plataforma</p>
+            <p><span class="material-icons">insert_invitation</span>Tareas personalizadas a los boletines</p>
+            <p><span class="material-icons">perm_phone_msg</span>Soporte personalizado</p>
           </CardText>
           <Button id="elegir3" className="btn-elegir">Elegir</Button>
         </CardBody>
