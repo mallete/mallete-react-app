2208591d3072c0d403cb99541c34695798bdd74e
Misa-El <mmancillaglz07@gmail.com>
2021-10-08 12:37:31 -0500

    cambio de clase en footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer () {
     <footer className='principal-container container-fluid'>
       <div className='links-container d-flex justify-content-between mb-2'>
         <p className=' copyright mt-3'> © 2021 Mallete</p>
-        <a className='link-footer mt-3' href='#'>Terminos y condiciones</a>
+        <a className='link-footer-component mt-3' href='#'>Terminos y condiciones</a>
       </div>
     </footer>
   )
