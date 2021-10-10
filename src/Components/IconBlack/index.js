6dc60550d85db8adfe2f4cc9b95fc34247f7f45a
MiguelAngelMG42 <MikeRen@outlook.com>
2021-10-10 01:04:46 -0500

    Se agrega responsividad en login,registro y redireccionamiento

--- src/Components/IconBlack/index.js
+++ src/Components/IconBlack/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./style.scss";
 import iconMallete from "../../Assets/Icons/mallete-black.png"
+import { NavbarBrand } from "reactstrap";
 
 const IconBlack = () =>{
     return(
         <>
-        <div className="icon-container" >
+        <NavbarBrand href="/" className="icon-container" >
         <img className="icon-img" type="icon" src={iconMallete}></img>
-        </div>
+        </NavbarBrand>
         </>
     );
 }
