4eb98df5fdf276d022012e938252c5795e4be77b
alfredopizana <pizana.alfredo@gmail.com>
2021-10-11 02:40:05 -0500

    created Generic Table

---
+++ src/Components/GenericTable/index.js
@@ -0,0 +1,15 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+
+import React, { useState,useEffect } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const GenericTable = (props) => {
+    const {a} = props;
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default GenericTable;
