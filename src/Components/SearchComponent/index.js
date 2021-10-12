b210391eb67d6f3a327d0083af56f3f990d21c71
Misa-El <mmancillaglz07@gmail.com>
2021-10-12 18:42:50 -0500

    componente de busqueda

---
+++ src/Components/SearchComponent/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+const SearchComponent = (props) => {
+const {handler} = props
+    return (
+        <div className="container">
+            <div className="input-group mb-1 input-search mr-auto" >
+                <input type="text" class="form-control" placeholder="Actor / Demandado / Expediente" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={handler}/>
+                <div className="input-group-append">
+                    <button className=" btn-search" type="button" id="button-addon2"><span class="material-icons">gavel</span></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchComponent
