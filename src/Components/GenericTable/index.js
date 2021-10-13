d6efd8411e39fe16df056c17f52e3f2acbb918cc
AlfredoPizana <pizana.alfredo@gmail.com>
2021-10-12 19:11:57 -0500

    Added app permissions

--- src/Components/GenericTable/index.js
+++ src/Components/GenericTable/index.js
@@ -1,11 +1,10 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
-import React, { useState,useEffect } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { Table } from 'reactstrap';
+import React, { useState, useEffect } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Table } from 'reactstrap'
 
 const GenericTable = (props) => {
-    /**
+  /**
      * @tableData
      * tableData = {
      *  headers: [
@@ -27,25 +26,23 @@ const GenericTable = (props) => {
      *      }
      *  ]
      */
-    const { headers, rows } = props
-    console.log({ headers, rows })
-    return (
-        <>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                    </tr>
-                </thead>
-                <tbody>
-
-                </tbody>
-            </Table>
-        </>
-    );
+  const { headers, rows } = props
+  console.log({ headers, rows })
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody />
+      </Table>
+    </>
+  )
 }
 
-export default GenericTable;
+export default GenericTable
